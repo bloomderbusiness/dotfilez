2a7a2a402a7e0e2ffd96ce33bb5588601e86dcbd
bloomderbusiness <bloomderbusiness@gmail.com>
2026-04-16 17:24:00 +0200

    Update and rename requirements.txt to requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo pacman -Syyu && sudo pacman -S sway foot rofi waybar starship fish swaybg swaylock swayidle rofi-wayland wl-clipboard cliphist wireplumber pavucontrol networkmanager otf-font-awesome ttf-jetbrains-mono-nerd noto-fonts noto-fonts-cjk noto-fonts-emoji fnm firefox steam dolphin ark mpv feh git micro jdk-openjdk fuse opencode podman podman-compose ufw timeshift mako
