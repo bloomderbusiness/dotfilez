c12b4a791d7363cefcca12a5a621afb18d414159
bloomderbusiness <bloomderbusiness@gmail.com>
2026-04-18 13:07:35 +0200

    Modify requirements script for file copying and packages
    Updated the script to copy all files to the home directory and modified the package installation list to include 'code' while removing Sublime Text installation steps.

--- requirements.sh
+++ requirements.sh
@@ -2,21 +2,15 @@
 
 # Copy dotfiles
 echo "Copy dotfiles.."
-cp -r dotfilez/* "$HOME/"
+cp -r /* "$HOME/"
 
 # Install requirements
 echo "Install requirements.."
-sudo pacman -Syyu && sudo pacman -S foot rofi waybar starship fish swaybg swaylock swayidle rofi-wayland wl-clipboard cliphist wireplumber pavucontrol networkmanager otf-font-awesome ttf-jetbrains-mono-nerd noto-fonts noto-fonts-cjk noto-fonts-emoji fnm firefox nautilus 7zip mpv feh git micro jdk-openjdk fuse opencode podman podman-compose ufw timeshift mako
+sudo pacman -Syyu && sudo pacman -S foot rofi waybar starship fish swaybg swaylock swayidle rofi-wayland wl-clipboard cliphist wireplumber pavucontrol networkmanager otf-font-awesome ttf-jetbrains-mono-nerd noto-fonts noto-fonts-cjk noto-fonts-emoji fnm firefox nautilus 7zip mpv feh git jdk-openjdk fuse opencode podman podman-compose ufw timeshift mako code
 
 # Enable Firewall
 echo "Enable Firewall.."
 sudo ufw enable
-
-# Install Sublime Text
-echo "Install Sublime Text.."
-curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
-echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
-sudo pacman -Syyu sublime-text
 
 # Install AUR Helper (PARU)
 echo "Install AUR Helper (PARU).."
