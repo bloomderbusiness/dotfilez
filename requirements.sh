f94d56fc59a0c30bde874200d5b95f1fc7a5e993
bloomderbusiness <bloomderbusiness@gmail.com>
2026-04-30 16:32:03 +0200

    Add docker and docker-compose to installation requirements

--- requirements.sh
+++ requirements.sh
@@ -9,7 +9,7 @@ cp -r Images/ "$HOME/"
 
 # Install requirements
 echo "Install requirements.."
-sudo pacman -Syyu --noconfirm && sudo pacman -S --noconfirm sway foot rofi waybar starship fish swaybg swaylock swayidle rofi-wayland wl-clipboard cliphist wireplumber pavucontrol networkmanager otf-font-awesome ttf-jetbrains-mono-nerd noto-fonts noto-fonts-cjk noto-fonts-emoji fnm firefox nautilus 7zip mpv feh git jdk-openjdk fuse opencode podman podman-compose ufw timeshift mako code fastfetch
+sudo pacman -Syyu --noconfirm && sudo pacman -S --noconfirm sway foot rofi waybar starship fish swaybg swaylock swayidle rofi-wayland wl-clipboard cliphist wireplumber pavucontrol networkmanager otf-font-awesome ttf-jetbrains-mono-nerd noto-fonts noto-fonts-cjk noto-fonts-emoji fnm firefox nautilus 7zip mpv feh git jdk-openjdk fuse docker docker-compose ufw timeshift mako code fastfetch
 
 # Enable Firewall
 echo "Enable Firewall.."
